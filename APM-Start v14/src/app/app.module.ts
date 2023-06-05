26d8cd91750229a25b4ee1611e2b47ff3ce97225
Nitroceliulioze <aksionova.diana@gmail.com>
2023-06-05 17:21:52 +0300

    lazy loading

--- APM-Start v14/src/app/app.module.ts
+++ APM-Start v14/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 /* Feature Modules */
-import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
 
@@ -24,11 +23,9 @@ import { MessageModule } from './messages/message.module';
     BrowserAnimationsModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 500 }),
-    ProductModule,
     UserModule,
     MessageModule,
-    AppRoutingModule,
-    
+    AppRoutingModule,    
   ],
   declarations: [
     AppComponent,
