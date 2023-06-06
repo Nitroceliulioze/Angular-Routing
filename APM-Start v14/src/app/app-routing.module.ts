6bc209dcb0dfe3de7874e68b922b8cfa38e62046
Nitroceliulioze <aksionova.diana@gmail.com>
2023-06-06 10:02:13 +0300

    eager lazy loading  (preloading)

--- APM-Start v14/src/app/app-routing.module.ts
+++ APM-Start v14/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 @NgModule({
   imports: [
@@ -13,12 +14,13 @@ import { AuthGuard } from './user/auth.guard';
         {
           path: 'products',
           canActivate: [AuthGuard],
+          data: { preload: true },
           loadChildren: () =>
             import('./products/product.module').then((m) => m.ProductModule),
         },
         { path: '', redirectTo: 'welcome', pathMatch: 'full' },
         { path: '**', component: PageNotFoundComponent },
-      ]
+      ], { preloadingStrategy: SelectiveStrategy }
       // { enableTracing: true } console rodo visus eventus
     ),
   ],
--- APM-Start v14/src/app/user/auth.guard.ts
+++ APM-Start v14/src/app/user/auth.guard.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private authService: AuthService, private router: Router) {}
+
+  canLoad(route: Route): boolean {
+    return this.checkedLoggedIn(route.path)
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -16,7 +20,7 @@ export class AuthGuard implements CanActivate {
     return this.checkedLoggedIn(state.url);
   }
 
-  checkedLoggedIn(url: string): boolean {
+  checkedLoggedIn(url: string | undefined): boolean {
     if(this.authService.isLoggedIn) {
       return true;
     }
